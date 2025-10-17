8f58d42f5bb29a7f0f615de21f045a25605d587c
paulr <paulriff.prb@gmail.com>
2025-10-16 20:28:32 -0400

    Implementacion del olvidar contraseña

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import Dashboard from './pages/Dashboard';
 import ManageUsersPage from './pages/ManageUsersPage';
 import MainLayout from './layouts/MainLayout';
 import ProtectedRoute from './components/auth/ProtectedRoute';
-// Asegúrate de que esta línea apunta al archivo .jsx
 import AdminRoute from './components/auth/AdminRoute.jsx';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 import './styles/App.css';
 
 function App() {
-  return (
-    <Routes>
-      {/* Rutas públicas */}
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
+    return (
+        <Routes>
+            {/* Rutas públicas */}
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/signup" element={<SignUpPage/>}/>
+            <Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+            <Route path="/reset-password/:uidb64/:token" element={<ResetPasswordPage/>}/>
+            {/* Rutas protegidas */}
+            <Route
+                path="/"
+                element={<ProtectedRoute><MainLayout/></ProtectedRoute>}
+            >
+                <Route index element={<Navigate to="/dashboard"/>}/>
+                <Route path="dashboard" element={<Dashboard/>}/>
 
-      {/* Rutas protegidas */}
-      <Route
-        path="/"
-        element={<ProtectedRoute><MainLayout /></ProtectedRoute>}
-      >
-        <Route index element={<Navigate to="/dashboard" />} />
-        <Route path="dashboard" element={<Dashboard />} />
-
-        {/* Rutas de Admin */}
-        <Route path="admin" element={<AdminRoute />}>
-          <Route path="users" element={<ManageUsersPage />} />
-        </Route>
-      </Route>
-    </Routes>
-  );
+                {/* Rutas de Admin */}
+                <Route path="admin" element={<AdminRoute/>}>
+                    <Route path="users" element={<ManageUsersPage/>}/>
+                </Route>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
